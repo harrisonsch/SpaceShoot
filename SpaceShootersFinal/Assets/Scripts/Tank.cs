ffcd7552b1d555509258e7bf8bb823188688fe22
harrisonsch <harrisonschneider1@gmail.com>
2024-04-23 14:35:11 -0400

    added Tank

---
+++ SpaceShootersFinal/Assets/Scripts/Tank.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Tank", menuName = "PowerUp/Other/Tank")]
+public class Tank : PowerUp
+{
+         private float oldHP = 100f;
+    public Tank()
+    {
+        powerUpName = "Tank";
+        duration = 999f; 
+        cost = 3f;
+        description = "Triples your maximum health";
+        value = 20f;
+    }
+
+    public override void Activate()
+    {
+        oldHP = GameController.Instance.baseHealth;
+        GameController.Instance.baseHealth = oldHP * 3;
+        GameController.Instance.health = GameController.Instance.health * 3;
+    }
+
+    public override void Deactivate()
+    {
+        GameController.Instance.baseHealth = oldHP;
+    }
+}
