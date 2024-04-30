31f042e16c46fcf30179d747200c0041caffe5a9
harrisonsch <harrisonschneider1@gmail.com>
2024-04-30 17:22:23 -0400

    final various bugfixes

--- SpaceShootersFinal/Assets/Scripts/walldestroy.cs
+++ SpaceShootersFinal/Assets/Scripts/walldestroy.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class walldestroy : MonoBehaviour
 {
+        public TextMeshProUGUI finalText;
     // Start is called before the first frame update
      private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+                Debug.Log("walltag is " + gameObject.tag);
+                if(gameObject.tag =="finalWall") {
+                        Debug.Log("enabling");
+                finalText.enabled = true;
+                finalText.gameObject.SetActive(true);
+            }
             Destroy(gameObject); // Destroy the GameObject this script is attached to
         }
     }
