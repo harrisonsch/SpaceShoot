e0d0066c120609a22faf92bbac21bebb82cf0f5b
harrisonsch <harrisonschneider1@gmail.com>
2024-05-03 11:22:16 -0400

    fixed health bars

--- SpaceShootersFinal/Assets/Scripts/EnemyMonster.cs
+++ SpaceShootersFinal/Assets/Scripts/EnemyMonster.cs
@@ -13,19 +13,23 @@ public class EnemyMonster : MonoBehaviour
     public AudioSource boom; // Audio source component for playing sound effects
     private bool died = false;
     private Rigidbody rb;
-    [SerializeField] private floatinghealthbar healthBar;
+    
+    public HealthBar healthBar;
 
     private void Start()
     {
         startHealth = health;
         rb = GetComponent<Rigidbody>();
-        healthBar.UpdateHealth(health, startHealth);
+        healthBar.SetHealth(health, startHealth);
     }
 
     public void Damage(float value)
     {
         health -= value;
-        healthBar.UpdateHealth(health, startHealth);
+       if (healthBar != null)
+        {
+                healthBar.SetHealth(health, startHealth); 
+        }
 
         // Display damage indicator
         DamageIndicator indicator = Instantiate(damageText, spawnPos.position, Quaternion.identity).GetComponent<DamageIndicator>();
